1cba92a91b279b9a1870353a72bdd1b7b7e9e266
Kirill Udaltsov <commits@udaltsovk.ru>
2025-08-21 16:51:47 +0300

    chore(flake): disallow unfree packages

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -9,10 +10,9 @@
   };
   outputs = inputs:
     inputs.flake-utils.lib.eachDefaultSystem (system: let
-      overlays = [(import inputs.rust-overlay)];
       pkgs = import inputs.nixpkgs {
-        inherit system overlays;
-        config.allowUnfree = true;
+        inherit system;
+        overlays = [(import inputs.rust-overlay)];
       };
     in {
       devShell = import ./shell.nix {inherit pkgs;};
