160c426af506868e395beadf8dab5148bd6506a4
Kirill Udaltsov <commits@udaltsovk.ru>
2025-10-03 09:12:13 +0300

    refactor(bootstrap): move BootstrapperExt trait into a derive macro

--- apps/ad_platform/bootstrap/monolyth/src/bootstrappers/mod.rs
+++ apps/ad_platform/bootstrap/monolyth/src/bootstrappers/mod.rs
@@ -1,10 +1,7 @@
-use async_trait::async_trait;
+use lib::bootstrap::bootstrapper_ext_trait;
 
 use crate::Modules;
 
 pub mod rest_api;
 
-#[async_trait]
-pub trait BootstraperExt {
-    async fn bootstrap(modules: Modules);
-}
+bootstrapper_ext_trait!(Modules);
--- packages/lib-backend/bootstrap/src/lib.rs
+++ packages/lib-backend/bootstrap/src/lib.rs
@@ -1,2 +1,12 @@
 #[cfg(feature = "openapi")]
 pub mod openapi;
+
+#[macro_export]
+macro_rules! bootstrapper_ext_trait {
+    ($modules_ty: ty) => {
+        #[async_trait::async_trait]
+        pub trait BootstraperExt {
+            async fn bootstrap(modules: $modules_ty);
+        }
+    };
+}
