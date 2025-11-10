741214c2b581e57fab9bb95707fe05eb643fe6d5
Kirill Udaltsov <commits@udaltsovk.ru>
2025-11-10 08:23:18 +0300

    fix(lib-backend/bootstrap)!: typo: BootstraperExt -> BootstrapperExt

--- packages/lib-backend/bootstrap/src/bootstrapper_ext.rs
+++ packages/lib-backend/bootstrap/src/bootstrapper_ext.rs
@@ -2,7 +2,7 @@
 macro_rules! bootstrapper_ext_trait {
     ($modules_ty: ty) => {
         #[async_trait::async_trait]
-        pub trait BootstraperExt {
+        pub trait BootstrapperExt {
             async fn bootstrap(modules: $modules_ty);
         }
     };
