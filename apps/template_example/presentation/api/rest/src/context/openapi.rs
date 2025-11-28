ca75b438cc4132092dbd8f73d9ba3b98907604da
Kirill Udaltsov <commits@udaltsovk.ru>
2025-11-28 08:37:48 +0300

    refactor: openapi

--- apps/template_example/presentation/api/rest/src/context/openapi.rs
+++ apps/template_example/presentation/api/rest/src/context/openapi.rs
@@ -1,14 +1,24 @@
-use utoipa::OpenApi;
+use utoipa::OpenApi as OpenApiDerive;
 
 use crate::routes::clients::CLIENTS_TAG;
 
-#[derive(OpenApi)]
+#[derive(OpenApiDerive)]
 #[openapi(
     info(
-        title = "Advertisement Platform API"
+        title = "Example Template API"
     ),
     servers(
-        (url = "http://localhost:8080/v0", description = "Local instance (v0)"),
+        (
+            url = "/{api_version}",
+            description = "Default server",
+            variables(
+                ("api_version" = (
+                    description = "Api version to use",
+                    enum_values("v0"),
+                    default = "v0",
+                )),
+            ),
+        ),
     ),
     tags(
         (name = CLIENTS_TAG, description = ""),
