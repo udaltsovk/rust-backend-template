b289aad11e6eb5b9c7b4b9e1822dd8fd77abc2bb
Kirill Udaltsov <commits@udaltsovk.ru>
2025-12-02 09:08:16 +0300

    fix: migrations

--- apps/template_example/infrastructure/persistence/postgres/migrations/20250625122245_clients.up.sql
+++ apps/template_example/infrastructure/persistence/postgres/migrations/20250625122245_clients.up.sql
@@ -1,10 +1,17 @@
-CREATE TYPE client_gender AS ENUM ('male', 'female');
+DO
+$$
+    BEGIN
+        IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'client_gender') THEN
+            CREATE TYPE client_gender AS ENUM ('male', 'female');
+        END IF;
+    END
+$$;
 
 CREATE TABLE IF NOT EXISTS clients
 (
-    id       uuid        NOT NULL PRIMARY KEY,
-    login    varchar(32) NOT NULL,
-    age      int         NOT NULL CHECK ( age > 0 ),
+    id       uuid          NOT NULL PRIMARY KEY,
+    login    varchar(32)   NOT NULL CHECK ( length(login) >= 3 ),
+    age      int           NOT NULL CHECK ( age >= 0 AND age <= 255 ),
     gender   client_gender NOT NULL,
-    location text        NOT NULL
+    location varchar(100)  NOT NULL CHECK ( length(location) >= 5 )
 );
