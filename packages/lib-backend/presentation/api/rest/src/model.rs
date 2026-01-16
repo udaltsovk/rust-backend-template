329935974e5fa847480144259c59d7ee992c9777
Kirill Udaltsov <commits@udaltsovk.ru>
2026-01-16 14:40:35 +0300

    feat(lib-backend/presentation/api/rest): add validator for nested structs

--- packages/lib-backend/presentation/api/rest/src/model.rs
+++ packages/lib-backend/presentation/api/rest/src/model.rs
@@ -1,4 +1,7 @@
-use domain::validation::error::ValidationErrors;
+use std::{any::type_name, marker::PhantomData};
+
+use domain::validation::ValidationConfirmation;
+pub use domain::validation::error::ValidationErrors;
 
 pub trait ParseableJson<T> {
     fn parse(self) -> Result<T, ValidationErrors>;
@@ -22,6 +25,83 @@ where
             .collect::<ValidationErrors>()
             .into_result(|_| converted.into_iter().flatten().collect())
     }
+}
+
+pub struct NestedValidator<J, T>
+where
+    J: ParseableJson<T>,
+{
+    inner: Result<T, ValidationErrors>,
+    _phantom: PhantomData<J>,
+}
+
+impl<J, T> NestedValidator<J, T>
+where
+    J: ParseableJson<T>,
+{
+    pub fn new(value: J, errors: &mut ValidationErrors) -> Self {
+        let res: Result<T, ValidationErrors> = value.parse();
+
+        if let Err(ref err) = res {
+            errors.extend(err.clone());
+        }
+
+        Self {
+            inner: res,
+            _phantom: PhantomData,
+        }
+    }
+
+    pub fn validated(self, _confirmation: ValidationConfirmation) -> T {
+        self.inner.unwrap_or_else(|_| {
+            panic!(
+                "`{}` should be Ok because error vec is empty",
+                type_name::<Self>()
+            )
+        })
+    }
+}
+
+pub trait IntoNestedValidator<T>
+where
+    Self: ParseableJson<T> + Sized,
+{
+    fn into_nested_validator(
+        self,
+        errors: &mut ValidationErrors,
+    ) -> NestedValidator<Self, T>;
+}
+
+impl<J, T> IntoNestedValidator<T> for J
+where
+    J: ParseableJson<T> + Sized,
+{
+    #[inline]
+    fn into_nested_validator(
+        self,
+        errors: &mut ValidationErrors,
+    ) -> NestedValidator<J, T> {
+        NestedValidator::new(self, errors)
+    }
+}
+
+#[macro_export]
+macro_rules! into_nested_validators {
+    ($($field: expr),*) => {
+        {
+            #[allow(unused_imports)]
+            use $crate::model::{IntoNestedValidator as _, ValidationErrors};
+
+            #[allow(unused_mut)]
+            let mut errors = ValidationErrors::new();
+
+            let validators = ($(
+              $field.into_nested_validator(&mut errors)
+            ),*);
+
+            (errors, validators)
+        }
+    };
 }
 
 #[cfg(test)]
